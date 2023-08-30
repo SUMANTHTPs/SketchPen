bc804a1ab5f6df8f68faea3f9cac04ee1afd104f
SUMANTHTPs <sumanthtps@gmail.com>
2023-08-30 23:08:59 +0530

    Adding footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import DrawingCanvas from './components/DrawingCanvas';
 import Navbar from './components/Navbar';
 import { DrawingContextProvider } from './context/drawingContext';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <DrawingContextProvider>
       <Navbar />
       <DrawingCanvas />
+      <Footer />
     </DrawingContextProvider>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>Copyrights &copy; Sumanth T P</footer>;
+};
+
+export default Footer;
