0a218666b09c793405a595d6bd126140134f68ed
SUMANTHTPs <sumanthtps@gmail.com>
2023-08-30 22:57:34 +0530

    solving lint

--- src/components/DrawingCanvas.jsx
+++ src/components/DrawingCanvas.jsx
@@ -34,6 +34,7 @@ const DrawingCanvas = () => {
 
   useEffect(() => {
     setCanvasContext();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleBackgroundColorChange = (event) => {
