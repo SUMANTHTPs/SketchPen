be8d96bd8acd598c867d607168dda7a9f8423110
SUMANTHTPs <sumanthtps@gmail.com>
2023-08-30 11:59:05 +0530

    Adding toggle and CSS

--- src/components/DrawingCanvas.jsx
+++ src/components/DrawingCanvas.jsx
@@ -8,6 +8,7 @@ const DrawingCanvas = () => {
   const [backgroundColor, setBackgroundColor] = useState("#ffffff");
   const [penColor, setPenColor] = useState("#000000");
   const [penSize, setPenSize] = useState(1);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -76,10 +77,10 @@ const DrawingCanvas = () => {
 
   const handlePenSize = (e) => {
     setPenSize(e.target.value);
-  }
+  };
 
   return (
-    <div>
+    <div className="container">
       <canvas
         ref={canvasRef}
         onMouseDown={startDrawing}
@@ -91,26 +92,56 @@ const DrawingCanvas = () => {
           backgroundColor,
           borderRadius: "16px",
         }}
+        onClick={() => setToggle(false)}
       ></canvas>
-      <div>
-        <SketchPicker
-          color={backgroundColor}
-          onChange={(color) => {
-            setBackgroundColor(color.hex);
-          }}
-        />
+      <div className="options">
+        <div>
+          {toggle && (
+            <SketchPicker
+              color={penColor}
+              onChange={(color) => {
+                setPenColor(color.hex);
+                context.strokeStyle = color.hex;
+              }}
+              className="sketch-picker"
+            />
+          )}
+          <div onClick={() => setToggle(!toggle)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill={penColor}
+              className="bi bi-pencil-fill pen"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <input
+            type="color"
+            value={backgroundColor}
+            onChange={handleBackgroundColorChange}
+            className="background-color-btn"
+            style={{
+              padding: "0",
+              border: "none",
+              width: "24px",
+              height: "28px",
+            }}
+          />
+        </div>
+        <button onClick={clearCanvas}>Clear</button>
+        <button onClick={downloadCanvas}>Download as Image</button>
+        <select name="PenSize" id="PenSize" onChange={handlePenSize}>
+          <option value={1}>x-small</option>
+          <option value={3}>small</option>
+          <option value={5}>medium</option>
+          <option value={10}>Large</option>
+        </select>
       </div>
-      <div>
-        <label>Pen Color: </label>
-        <input type="color" value={penColor} onChange={handlePenColorChange} />
-      </div>
-      <button onClick={clearCanvas}>Clear</button>
-      <button onClick={downloadCanvas}>Download as Image</button>
-      <select name="PenSize" id="PenSize" onChange={handlePenSize}>
-        <option value={1}>Small</option>
-        <option value={3}>Medium</option>
-        <option value={10}>Large</option>
-      </select>
     </div>
   );
 };
